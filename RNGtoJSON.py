cd76a0ff59c6c010c51bff9f56c4552077763fd8
JoshRyan <thejoshgarber@gmail.com>
2025-02-19 15:51:40 -0500

    Changed JSON structure, more descriptive action keys

--- RNGtoJSON.py
+++ RNGtoJSON.py
@@ -10,17 +10,19 @@ def is_json(text):
   except json.JSONDecodeError:
     return False
 def number_to_action(number):
-   if number == '0' or 0:
-      return 'Fold'
-   elif number == '1' or 1:
-      return 'Call'
-   elif number == '3' or 3:
-      return 'ALLIN'
-   elif number == '5' or 5:
+   if number == '0':
+      return "Fold"
+   elif number == '1':
+      return "Call"
+   elif number == '3':
+      return "ALLIN"
+   elif number == '5':
       return "Min"
+   # elif number == '15':
+   #    return ""
    else:
       a = number.split('00')
-      return ('Raise '+str(a[-1]+'%'))
+      return str(number)
 def rngtodict(file_path):
     d = {}
     with open(file_path, 'r') as f:
@@ -40,7 +42,7 @@ def name_node(node):
       return 'root'
 
 raise_size = 0
-number_converter = {0: "Fold", 1: "Call", 5: "Min", 3: "ALLIN", 4: "Raise "+str(raise_size)+"%"} #5.0.0.0.40075.0 meant raise 75% pot
+number_converter = {0: "Fold", 1: "Call", 5: "Min", 3: "ALLIN", 4: "Raise "+str(raise_size)+"%", 2: "Raise X"} #5.0.0.0.40075.0 meant raise 75% pot
 folder_path = PATH.folder_path
 all_files = glob.glob(os.path.join(folder_path, "*.rng"))
 players = os.path.basename(folder_path).split("_")
@@ -74,15 +76,15 @@ for file_path in all_files:
    #print(is_json(str(json.dumps(action_dict))))
    with open(os.path.join('C:\PythonStuff\MonkerPythonGUI\\'+output_dir,name_node(node)+'.json'),'a') as file:
          if node_count[name_node(node)] == 0:
-            file.write('[\n')
-         file.write(str(json.dumps(action_dict))+','+'\n')
+            file.write('{')
+         file.write('"'+str(number_to_action(action))+'"'+":"+str(json.dumps(rngtodict(file_path)))+ ',')
 
+print(node_list)
 for node in node_list:
    with open(os.path.join('C:\PythonStuff\MonkerPythonGUI\\'+output_dir,name_node(node)+'.json'),'r') as file:
-         data = file.read()[:-2]
+      data = file.read()[:-1]
    with open(os.path.join('C:\PythonStuff\MonkerPythonGUI\\'+output_dir,name_node(node)+'.json'), 'w') as file:
-      file.write(data+'\n]')
-      #print(is_json(data+'\n]'))
-      if is_json(data+'\n]') == False:
+      file.write(data+"}")
+      if is_json(data+"}") == False:
          print(str(node)+' is false')
          break
